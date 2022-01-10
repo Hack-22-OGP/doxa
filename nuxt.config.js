5b3f92b20dfbb160f36c121b8fbcd257e0576a33
ken <kenneth_lee@mom.gov.sg>
2022-01-11 03:35:43 +0800

    Add in tailwind as UI framework

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,14 @@
 export default {
   srcDir: 'src/',
+  configPath: '~~/tailwind.config.js',
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  buildModules: ['@nuxt/postcss8'],
+  css: ['./src/assets/css/tailwind.css'],
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Hello world!</h1>
+  <div
+    class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+  >
+    <div class="flex-shrink-0">
+      <img class="h-12 w-12" src="~/assets/img/chat.png" alt="chat icon" />
+    </div>
+    <div>
+      <div class="text-xl font-medium text-black">Welcome onboard!</div>
+      <p class="text-gray-500">Welcome to DOXA! Enjoy ypur stay.</p>
+    </div>
+  </div>
 </template>
