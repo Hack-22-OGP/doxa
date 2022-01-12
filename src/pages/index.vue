b8caf5000d3a5329686ad4c732dca14dcdf7fe09
ken <kenneth_lee@mom.gov.sg>
2022-01-13 00:06:42 +0800

    Typo master at work

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+    class="flex items-center max-w-sm p-6 mx-auto bg-white shadow-md rounded-xl space-x-4"
   >
     <div class="flex-shrink-0">
       <img class="h-12 w-12" src="~/assets/img/chat.png" alt="chat icon" />
     </div>
     <div>
       <div class="text-xl font-medium text-black">Welcome onboard!</div>
-      <p class="text-gray-500">Welcome to DOXA! Enjoy ypur stay.</p>
+      <p class="text-gray-500">Welcome to DOXA! Enjoy your stay.</p>
     </div>
   </div>
 </template>
