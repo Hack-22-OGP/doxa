d483209cf1469bcc17a62e99caaff5d2fef196e8
ken <kenneth_lee@mom.gov.sg>
2022-01-17 10:46:14 +0800

    Add in split pages for poll create and vote

---
+++ src/pages/poll/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <PollCreate v-if="!this.id" />
+  <PollVote v-else />
+</template>
+
+<script>
+import PollCreate from '@/components/PollCreate.vue'
+import PollVote from '@/components/PollVote.vue'
+
+export default {
+  components: { PollCreate, PollVote },
+  async asyncData({ params }) {
+    return { id: params.id }
+  },
+}
+</script>
