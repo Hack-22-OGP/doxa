3c2e44b047b05970ee6a7b9c4b39336881b2bb1e
ken <kenneth_lee@mom.gov.sg>
2022-01-21 10:34:37 +0800

    Add a log for debug on cookies

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,6 +1,8 @@
 export default function ({ app, route, redirect }) {
   const u = app.$cookies.get('u')
 
+  console.log('\n🐸🐸🐸 authenticate.js cookie?', u) // >>> TO-REMOVE
+
   if (!u) {
     return redirect(
       `${process.env.apiURL}/auth?target=${process.env.baseURL}${route.fullPath}`
